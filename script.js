8df8e9ffbbedca7021d0f17b2ba2dd95d3ae126a
Sonechka M <s212288s@gmail.com>
2020-01-06 07:12:46 -0500

    added functionality, written out all steps and left questions in pseudo code

--- script.js
+++ script.js
@@ -1,79 +1,68 @@
+// connect to the #gen-btn
+    var genBtn = document.querySelector("#gen-btn");
 // create arrays for characters
-    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    //var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var numbers = ("123456789").split("");
     var special = [];
     var letters = ("abcdefghijklmnopqrstuvwxyz").split("");
     var upper = [];
 
-    console.log("NUMBERS")
-    console.log(numbers)
+    genBtn.addEventListener("click", function() {
+        // prompt user to choose password length
+        var length = parseInt(prompt("Chose a number between 8 and 128"));
+        // ask if they want numbers
+        var numbersTrue = confirm("Would you like to have numbers?");
+        // ask if they want letters
+        var lettersTrue = confirm("Would you like to have letters?");
+      
+        // empty array of possible characters
+        var possChar = [];
+        // empty array for password
+        var password = [];
+      
+        // if numbers wanted concat numbers with possChar
+        if (numbersTrue) {
+          possChar = possChar.concat(numbers);
+        }
+        
+        //if letters wanted concat letters with possChar
+        if (lettersTrue) {
+          possChar = possChar.concat(letters);
+        }
+        
+        // loop through possChar password length amount of times, pick random characters and push them into password
+        for (var i = 0; i < length; i++) {
+          var random = Math.floor(Math.random() * possChar.length);
+          password.push(possChar[random]);
+        }
+        
+        // join characters in password in the password and replace all commas by empty spaces
+        password = password.join().replace(/,/g, "");
+        
+        console.log(password);
+        //result.value = password;
+      });
 
-    console.log("LETTERS");
-    console.log(letters);
-
-// prompt the user to choose password.length between 8 and 128 
-// var length = prompt("Choose password length between 8 and 128");
-// if length is less than 8 say min is 8
-// if length is more than 128 say max is 128
-
-// can you write a function that will go through elements inside of the arrays inside of this obejcet password.length amount of times and push only characters return randomzied password as a string?
-
-// can I create an empty object and push arrays into it under certain conditions?
-
-
-// length = 10
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c']
-
-// result = ['a', 5, 9, 'c']
-
-// console.log("LETTERS");
-// console.log(letter)
-
-
-
-// if user chooses to have numbers
-// push random numbers into numbers 9 times
-// for(var i = 0; i < 9; i++){
-//     var num = Math.floor(Math.random()*10);
-//     numbers.push(num);
-// }
-
-
-
-// if the user chooses to have letters
-// go through letters a certain number of times and push a certain number of letters into password
-
-
-
-
-// for(var i = 0; i < 8; i++){
-//     var letter = Math.floor(Math.random() * 27);
-//     letters.push(letter);
-// }
-
-
-// ====== PROMPTS TO CHOOSE LENGTH AND CHARACTERS =======
-
-
+// ================== PROMPTS TO CHOOSE CHARACTERS ==================
 
 // prompt the user to say chose if they want special characters
-// var specialChar = prompt("Would you like your password to contain special characters?");
-
-// prompt the user to say chose if they want numeric characters
-// var specialChar = prompt("Would you like your password to contain special characters?");
-
-// prompt the user to say chose if they want lowercase characters
-// var lowlChar = prompt("Would you like your password to contain lowercase characters?");
+// var special = prompt("Would you like your password to contain special characters?");
 
 // prompt the user to say chose if they want uppercase characters
-// var upChar = prompt("Would you like your password to contain uppercase characters?");
+// var upper = prompt("Would you like your password to contain uppercase characters?");
 
-// ============ VALIDATION =============
+// ========================= VALIDATION ==============================
+
+// if length is less than 8 say min is 8
+// if length is more than 128 say max is 128
 
 // make sure at least one character type is selected
 // what happens when Cancel is pressed? if?
 
-// =============== DOM ==============
+// when password is created make sure it gets characters from each character type selected
 
-// connect gen-btn to function
+// ============================= DOM =============================
+
 // display password in password box (textContent)
 // add copy to clipboard (clipboard interaction)
+// make sure password box gets reset
